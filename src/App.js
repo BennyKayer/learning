34bbb736af642f2395945a5ed8b05ac371508568
Paweł Benkowski <pawlo97.pb@gmail.com>
2020-09-25 09:33:59 +0200

    Router push and no render state done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
 import "./App.css";
 import Content from "./Content";
+import { Route, Switch, Link } from "react-router-dom";
+
+const SomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to some page</h1>
+            <Link to="/">
+                <span>Go back</span>
+            </Link>
+        </div>
+    );
+};
 
 const App = () => {
-    return <Content />;
+    return (
+        <Switch>
+            <Route exact path="/" component={Content} />
+            <Route exact path="/some_page" component={SomePage} />
+        </Switch>
+    );
 };
 
 export default App;
