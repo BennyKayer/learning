c040a8c13375ef8c52f9977666fc649697555173
Paweł Benkowski <pawlo97.pb@gmail.com>
2020-11-20 14:30:04 +0100

    zad 2

--- zaj_1.R
+++ zaj_1.R
@@ -17,13 +17,16 @@ sort(dane)
 i = .25 * 12
 j = i + 1
 # 3.84 i 4.41
-q_1 = 3.84 + .25 * (4.41 - 3.84)
-print(q_1)
-me = 3.84 + .50 * (4.41 - 3.84)
-print(me)
-q_3 = 3.84 + .75 * (4.41 - 3.84)
-print(q_3)
+# to jest zle
+# q_1 = 3.84 + .25 * (4.41 - 3.84)
+# print(q_1)
+# me = 3.84 + .50 * (4.41 - 3.84)
+# print(me)
+# q_3 = 3.84 + .75 * (4.41 - 3.84)
+# print(q_3)
 quantile(dane, 0.25, type=6)
+quantile(dane, 0.50, type=6)
+quantile(dane, 0.75, type=6)
 # domyslnie jest typ 7
 
 
