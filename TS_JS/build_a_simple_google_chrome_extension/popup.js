801430020c0b641883f723e35b245733e20aa205
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-03-07 16:22:09 +0100

    Simple chrome extension

---
+++ TS_JS/build_a_simple_google_chrome_extension/popup.js
@@ -0,0 +1,16 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var checkPageButton = document.getElementById("clickIt");
+    checkPageButton.addEventListener(
+      "click",
+      function () {
+        chrome.tabs.getSelected(null, function (tab) {
+          alert("Hello..! It's my first chrome extension.");
+        });
+      },
+      false
+    );
+  },
+  false
+);
