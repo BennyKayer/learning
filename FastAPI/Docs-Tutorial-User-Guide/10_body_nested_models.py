6df89bbe94f925f828a5a43e2cd7ead55740ce80
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-07-15 16:19:26 +0200

    Finished Body Nested Models

---
+++ FastAPI/Docs-Tutorial-User-Guide/10_body_nested_models.py
@@ -0,0 +1,61 @@
+"""https://fastapi.tiangolo.com/tutorial/body-nested-models/
+"""
+from typing import Optional, Set, List, Dict
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl  # noqa E0611
+
+app = FastAPI()
+
+
+class Image(BaseModel):
+    """Submodel for nesting demonstration"""
+
+    url: HttpUrl
+    name: str
+
+
+class Item(BaseModel):
+    """Item for nesting example"""
+
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+    tags: Set[str] = set()
+    images: Optional[List[Image]] = None
+
+
+class Offer(BaseModel):
+    """Deep nesting example model"""
+
+    name: str
+    description: Optional[str]
+    price: float
+    items: List[Item]
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    """Create offer"""
+    return offer
+
+
+@app.post("/offers/many/")
+async def create_many_offers(offers: List[Offer]):
+    """Create many offers"""
+    return offers
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    """Endpoint for nesting demonstration"""
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+@app.post("/index_weights/")
+async def create_index_weights(weights: Dict[int, float]):
+    """Demonstrates that although JSON only sends string
+    validation of data to be int and float is performed
+    """
+    return weights
