113c0a5b1c462a65aaa4a4924f37288374725778
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-07-15 18:08:23 +0200

    Finished Header Parameters

---
+++ FastAPI/Docs-Tutorial-User-Guide/14_Header_Parameters.py
@@ -0,0 +1,26 @@
+"""https://fastapi.tiangolo.com/tutorial/header-params/
+"""
+from typing import List, Optional
+from fastapi import FastAPI, Header
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(
+    user_agent: Optional[str] = Header(None),
+    weird_header: Optional[str] = Header(None, convert_underscores=False),
+):
+    """Demonstrates usage of header params
+    User-Agent gets automatically converted to python variable user_agent
+    thus can be read here
+    weird header is weird and does not convert underscores to -
+    so the HTTP proxies will block him
+    """
+    return {"User-Agent": user_agent}
+
+
+@app.get("/x_token/")
+async def x_token(x_token: Optional[List[str]] = Header(None)):
+    """Demonstrates list usage for headers that may appear many times"""
+    return {"X-Token values": x_token}
