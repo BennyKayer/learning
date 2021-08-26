b8a21467fe43fc91028b491b9dab24c2a9cd7580
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-08-26 16:14:05 +0200

    Request Forms and Files done

---
+++ FastAPI/Docs-Tutorial-User-Guide/20_Request_Forms_and_Files.py
@@ -0,0 +1,28 @@
+"""https://fastapi.tiangolo.com/tutorial/request-forms-and-files/
+"""
+from fastapi import FastAPI, File, Form, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_file(
+    small_file: bytes = File(...),
+    big_file: UploadFile = File(...),
+    token: str = Form(...),
+):
+    """Demonstrates use of both File and Form params
+
+    Args:
+        small_file (bytes, optional): [description]. Defaults to File(...).
+        big_file (UploadFile, optional): [description]. Defaults to File(...).
+        token (str, optional): [description]. Defaults to Form(...).
+
+    Returns:
+        [type]: [description]
+    """
+    return {
+        "small_file_size": len(small_file),
+        "token": token,
+        "big_file_content_type": big_file.content_type,
+    }
